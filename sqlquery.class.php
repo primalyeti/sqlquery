c6d254a1d5d6b5d59a8bc7005157fbff6075fa48
primalyeti <primalyeti@hotmail.com>
2013-08-12 15:04:05 -0700

    Update, add inflection

--- sqlquery.class.php
+++ sqlquery.class.php
@@ -1,3 +1,4 @@
+<?
 class SQLQuery
 {
     protected $_dbObj = false;
@@ -28,7 +29,9 @@ class SQLQuery
 			array_shift( $params );
 		}
 		
+		Registry::get("profiler")->start_time( "mysql" );
 		$return = $this->_dbObj->query( $query, true, $params );
+		Registry::get("profiler")->stop_time( "mysql", $query . "\nParams: \n" . print_r( $params, true ) );
 		
 		return $return;
 	}
@@ -42,7 +45,9 @@ class SQLQuery
 			array_shift( $params );
 		}
 		
-		$return = $this->_dbObj->query( $query, false, $params );
+		Registry::get("profiler")->start_time( "mysql" );
+		$return = $this->_dbObj->query( $query, DBH_OBJ_DEFAULT, $params );
+		Registry::get("profiler")->stop_time( "mysql", $query . "\nParams: \n" . print_r( $params, true ) );
 		
 		return $return;
 	}
@@ -68,17 +73,51 @@ class SQLQuery
 	}
 }
 
-class PDOConn
+interface SQLConn
+{
+	function connect( $host, $username, $password, $dbname );
+	function isValid();
+	function clean( $string );
+	function query( $query, $isObj, $params );
+	function id();
+	function error();
+	function errno();
+	function errdesc();
+}
+
+abstract class SQLHandle implements SQLConn
 {
 	protected $_dbHandle = false;
     protected $_result;
     protected $_isConn = false;
-
+    
     public function __construct( $host, $username, $password, $dbname )
 	{
 		$this->connect( $host, $username, $password, $dbname );
 	}
-    
+	
+	public function isValid()
+    {
+    	return $this->_isConn;
+    }
+	
+	protected function put_error( $msg )
+    {
+	    if( ENVIRONMENT == "LIVE" )
+		{
+			error_log( $msg );
+		}
+		else
+		{
+			error_log( $msg );
+		}
+		
+		return;
+    }
+}
+
+class PDOConn extends SQLHandle
+{
     /** Connects to database **/
     public function connect( $host, $username, $password, $dbname )
 	{
@@ -96,12 +135,6 @@ class PDOConn
 	    $this->_dbHandle->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 		
 		$this->_isConn = true;
-    }
-	
-	protected function put_error( $msg )
-    {
-	   error_log( $msg );
-	   return;
     }
     
     public function isValid()
@@ -245,6 +278,7 @@ class PDOConn
 			{
 				for( $i = 0; $i < $numOfFields; ++$i )
 				{
+					$table[$i] = Inflection::singularize( $table[$i] );
 					$tempResults[$table[$i]][$field[$i]] = $row[$i];
 				}
 				array_push( $result, $tempResults );
@@ -282,14 +316,31 @@ class PDOConn
 
 class SQLResult
 {
-	protected $_isValid = false;
-	protected $_query 	= null;
-	protected $_params 	= null;
-	protected $_stmt 	= null;
-	protected $_results	= array();
-	protected $_pos 	= -1;
+	protected $_isValid 		= false;
+	protected $_query 			= null;
+	protected $_params 			= null;
+	protected $_stmt 			= null;
+	protected $_results			= array();
+	protected $_pos 			= -1;
+	protected $_wasSerialized 	= false;
 	
 	public function __construct(){}
+	
+	public function __sleep()
+	{
+		return array(
+			"_isValid",
+			"_query",
+			"_params",
+			"_results",
+			"_pos",
+		);
+	}
+	
+	public function __wakeup()
+	{
+		$this->_wasSerialized = true;
+	}
 	
 	/**
 	* 
@@ -308,7 +359,7 @@ class SQLResult
 			return false;
 		}
 		
-		return $this->row_as_object( 0 );
+		return $this->_results[0];
 	}
 	
 	public function last()
@@ -318,17 +369,18 @@ class SQLResult
 			return false;
 		}
 		
-		return $this->row_as_object( $this->length()-1 );
+		return $this->_results[$this->length()-1];
 	}
 	
 	public function curr()
 	{
-		if( !isset( $this->_results[$this->_pos] ) )
+		$pos = ( $this->_pos == -1 && $this->length() > 0 ? 0 : -1 );
+		if( !isset( $this->_results[$pos] ) )
 		{
 			return false;
 		}
 		
-		return $this->row_as_object( $this->_pos );
+		return $this->_results[ $this->_pos ];
 	}
 	
 	public function next()
@@ -337,8 +389,7 @@ class SQLResult
 		{
 			return false;
 		}
-		
-		return $this->row_as_object( ++$this->_pos );
+		return $this->_results[++$this->_pos];
 	}
 	
 	public function prev()
@@ -348,18 +399,12 @@ class SQLResult
 			return false;
 		}
 		
-		return $this->row_as_object( --$this->_pos );
+		return $this->_results[--$this->_pos];
 	}
 	
 	public function reset()
 	{
-		if( empty( $this->_results ) )
-		{
-			$this->_pos = -1;
-			return true;
-		}
-	
-		$this->_pos = 0;
+		$this->_pos = -1;
 		return true;
 	}
 
@@ -367,10 +412,35 @@ class SQLResult
 	{
 		return count( $this->_results );
 	}
-
+	
+	public function all( $table, $field )
+	{
+		// get position, then go to the beginning
+		$pos = $this->_pos;
+		$this->reset();
+		
+		// store the values
+		$vals = array();
+		while( $row = $this->next() )
+		{
+			if( isset( $this->$table ) && isset( $this->$table->$field ) && ( $val = $this->$table->$field ) != "" )
+			{
+				array_push( $vals, $val );
+			}
+		}
+		
+		$this->_pos = $pos;
+		
+		return $vals;
+	}
+	
 	public function error()
 	{
-		if( is_null( $this->_stmt ) )
+		if( $this->wasSerialized() && $this->isValid() )
+		{
+			return false;
+		}
+		else if( is_null( $this->_stmt ) )
 		{
 			return true;
 		}
@@ -400,8 +470,15 @@ class SQLResult
 	
 	public function as_array()
 	{
-		return $this->_results;
+		$resultArray = array();
+		foreach( $this->_results as $result )
+		{
+			array_push( $resultArray, $result->as_array() );
+		}
+
+		return $resultArray;
 	}
+
 	
 	/**
 	* 
@@ -430,8 +507,13 @@ class SQLResult
 		{
 			throw new Exception( "SQLResult: not a valid result array" );
 		}
+
+		foreach( $results as $result )
+		{
+			$row = new SQLRow( $result );
+			array_push($this->_results, $row );
+		}
 		
-		$this->_results = $results;
 		$this->_isValid = true;
 	}
 	
@@ -450,17 +532,37 @@ class SQLResult
 		return $this->_stmt;
 	}
 	
+	public function __get( $key )
+	{
+		if( !isset( $this->$key ) )
+		{
+			return $this->first()->$key;
+		}
+		
+		return null;
+	}
+	
 	/**
 	*
 	* PRIVATE
 	* 
 	**/
-	protected function row_as_object( $key )
+	protected function wasSerialized()
 	{
-		return $this->result_row_to_obj( $this->_results[$key] );
+		return $this->_wasSerialized;
+	}
+}
+
+class SQLRow
+{
+	protected $_row;
+
+	public function __construct( $arr )
+	{
+		$this->_row = $this->init( $arr );
 	}
 	
-	protected function result_row_to_obj( $arr )
+	private function init( $arr )
 	{
 		if( is_array( $arr ) ) 
 		{
@@ -473,7 +575,7 @@ class SQLResult
 			
 			foreach( $arr as $key => $val )
 			{
-				$new->{$key} = $this->result_row_to_obj( $val );
+				$new->{$key} = $this->init( $val );
 			}
 		}
 		else
@@ -483,4 +585,181 @@ class SQLResult
 		
 		return $new;
 	}
+	
+	public function __get( $key )
+	{
+		if( !isset( $this->_row->$key ) )
+		{
+			$this->_row->$key = $this->init( array() );
+		}
+		
+		return $this->_row->$key;
+	}
+	
+	public function as_array()
+	{
+		$array = array();
+		
+		foreach( $this->_row as $key => $value )
+		{
+			$array[$key] = (array) $value;
+		}
+		
+		return $array;
+	}
+}
+
+class Inflection
+{
+    static $plural = array(
+        '/(quiz)$/i'               => "$1zes",
+        '/^(ox)$/i'                => "$1en",
+        '/([m|l])ouse$/i'          => "$1ice",
+        '/(matr|vert|ind)ix|ex$/i' => "$1ices",
+        '/(x|ch|ss|sh)$/i'         => "$1es",
+        '/([^aeiouy]|qu)y$/i'      => "$1ies",
+        '/(hive)$/i'               => "$1s",
+        '/(?:([^f])fe|([lr])f)$/i' => "$1$2ves",
+        '/(shea|lea|loa|thie)f$/i' => "$1ves",
+        '/sis$/i'                  => "ses",
+        '/([ti])um$/i'             => "$1a",
+        '/(tomat|potat|ech|her|vet)o$/i'=> "$1oes",
+        '/(bu)s$/i'                => "$1ses",
+        '/(alias)$/i'              => "$1es",
+        '/(octop)us$/i'            => "$1i",
+        '/(ax|test)is$/i'          => "$1es",
+        '/(us)$/i'                 => "$1es",
+        '/s$/i'                    => "s",
+        '/$/'                      => "s"
+    );
+
+    static $singular = array(
+        '/(quiz)zes$/i'             => "$1",
+        '/(matr)ices$/i'            => "$1ix",
+        '/(vert|ind)ices$/i'        => "$1ex",
+        '/^(ox)en$/i'               => "$1",
+        '/(alias)es$/i'             => "$1",
+        '/(octop|vir)i$/i'          => "$1us",
+        '/(cris|ax|test)es$/i'      => "$1is",
+        '/(shoe)s$/i'               => "$1",
+        '/(o)es$/i'                 => "$1",
+        '/(bus)es$/i'               => "$1",
+        '/([m|l])ice$/i'            => "$1ouse",
+        '/(x|ch|ss|sh)es$/i'        => "$1",
+        '/(m)ovies$/i'              => "$1ovie",
+        '/(s)eries$/i'              => "$1eries",
+        '/([^aeiouy]|qu)ies$/i'     => "$1y",
+        '/([lr])ves$/i'             => "$1f",
+        '/(tive)s$/i'               => "$1",
+        '/(hive)s$/i'               => "$1",
+        '/(li|wi|kni)ves$/i'        => "$1fe",
+        '/(shea|loa|lea|thie)ves$/i'=> "$1f",
+        '/(^analy)ses$/i'           => "$1sis",
+        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i'  => "$1$2sis",
+        '/([ti])a$/i'               => "$1um",
+        '/(n)ews$/i'                => "$1ews",
+        '/(h|bl)ouses$/i'           => "$1ouse",
+        '/(corpse)s$/i'             => "$1",
+        '/(us)es$/i'                => "$1",
+        '/ss$/i'                    => "ss",
+        '/s$/i'                     => "",
+    );
+
+    static $irregular = array(
+        'move'   => 'moves',
+        'foot'   => 'feet',
+        'goose'  => 'geese',
+        'sex'    => 'sexes',
+        'child'  => 'children',
+        'man'    => 'men',
+        'tooth'  => 'teeth',
+        'person' => 'people',
+        'admin' => 'admin'
+    );
+
+    static $uncountable = array(
+        'sheep',
+        'fish',
+        'deer',
+        'series',
+        'species',
+        'money',
+        'rice',
+        'information',
+        'equipment'
+    );
+
+    public static function pluralize( $string )
+    {
+	// save some time in the case that singular and plural are the same
+        if( in_array( strtolower( $string ), self::$uncountable ) )
+		{
+            return $string;
+		}
+		
+         // check for irregular singular forms
+        foreach( self::$irregular as $pattern => $result )
+        {
+            $pattern = '/' . $pattern . '$/i';
+
+            if( preg_match( $pattern, $string ) )
+			{
+                return preg_replace( $pattern, $result, $string );
+			}
+        }
+
+        // check for matches using regular expressions
+        foreach( self::$plural as $pattern => $result )
+        {
+            if( preg_match( $pattern, $string ) )
+			{
+                return preg_replace( $pattern, $result, $string );
+			}
+        }
+
+        return $string;
+    }
+
+    public static function singularize( $string )
+    {	
+	 // save some time in the case that singular and plural are the same
+        if( in_array( strtolower( $string ), self::$uncountable ) )
+	{
+            return $string;
+	}
+		
+	// check for irregular plural forms
+        foreach( self::$irregular as $result => $pattern )
+        {
+            $pattern = '/' . $pattern . '$/i';
+
+            if( preg_match( $pattern, $string ) )
+			{
+                return preg_replace( $pattern, $result, $string );
+			}
+        }
+
+	    // check for matches using regular expressions
+        foreach( self::$singular as $pattern => $result )
+        {
+            if( preg_match( $pattern, $string ) )
+			{
+                return preg_replace( $pattern, $result, $string );
+			}
+        }
+
+        return $string;
+    }
+
+    public static function pluralize_if( $count, $string )
+    {
+        if( $count == 1 )
+		{
+            return "1 $string";
+		}
+        else
+		{
+            return $count . " " . self::pluralize( $string );
+		}
+    }
 }
