7bbb8224764055e704f4e8c6fa0f7b3de53c9dfe
primalyeti <primalyeti@hotmail.com>
2014-04-04 09:58:34 -0700

    Update sqlquery.class.php
    Update to framework version 1.1.9

--- sqlquery.class.php
+++ sqlquery.class.php
@@ -1,25 +1,41 @@
 <?
+<?
 class SQLQuery
 {
 	protected $_dbObj = false;
-    protected $_result;
+	protected $_result;
 
 	public function __construct( $db_host, $db_user, $db_password, $db_name )
 	{
 		$this->_dbObj = new PDOConn( $db_host, $db_user, $db_password, $db_name );
-		
+
 		if( $this->_dbObj->isValid() == false )
 		{
 			echo "Could not connect to DB";
 			return;
 		}
 	}
-	
+
+	public function beginTransaction()
+	{
+		return $this->_dbObj->beginTransaction();
+	}
+
+	public function commit()
+	{
+		return $this->_dbObj->commit();
+	}
+
+	public function rollBack()
+	{
+		return $this->_dbObj->rollBack();
+	}
+
 	public function clean( $string, $type = "str" )
 	{
 		return $this->_dbObj->clean( $string, $type );
 	}
-	
+
 	public function query_obj( $query )
 	{
 		$params = null;
@@ -28,12 +44,12 @@ class SQLQuery
 			$params = func_get_args();
 			array_shift( $params );
 		}
-		
-		$return = $this->_dbObj->query( $query, true, $params );
-		
+
+		$return = $this->_dbObj->query( $query, $params );
+
 		return $return;
 	}
-	
+
 	public function query( $query )
 	{
 		$params = null;
@@ -42,27 +58,27 @@ class SQLQuery
 			$params = func_get_args();
 			array_shift( $params );
 		}
-		
-		$return = $this->_dbObj->query( $query, true, $params );
-		
+
+		$return = $this->_dbObj->query( $query, $params );
+
 		return $return;
 	}
-	
+
 	public function id()
 	{
 		return $this->_dbObj->id();
 	}
-	
+
 	public function error()
 	{
 		return $this->_dbObj->error();
 	}
-	
+
 	public function errno()
 	{
 		return $this->_dbObj->errno();
 	}
-	
+
 	public function errdesc()
 	{
 		return $this->_dbObj->errdesc();
@@ -74,7 +90,7 @@ interface SQLConn
 	function connect( $host, $username, $password, $dbname );
 	function isValid();
 	function clean( $string );
-	function query( $query, $isObj, $params );
+	function query( $query, $params );
 	function id();
 	function error();
 	function errno();
@@ -84,30 +100,31 @@ interface SQLConn
 abstract class SQLHandle implements SQLConn
 {
 	protected $_dbHandle = false;
-    protected $_result;
-    protected $_isConn = false;
-    
-    public function __construct( $host, $username, $password, $dbname )
+	protected $_result;
+	protected $_isConn = false;
+
+	public function __construct( $host, $username, $password, $dbname )
 	{
 		$this->connect( $host, $username, $password, $dbname );
 	}
-	
+
 	public function isValid()
-    {
-    	return $this->_isConn;
-    }
-	
+	{
+		return $this->_isConn;
+	}
+
 	protected function put_error( $msg )
-    {
-	    error_log( $msg );
+	{
+		error_log( $msg );
+		
 		return;
-    }
+	}
 }
 
 class PDOConn extends SQLHandle
 {
-    /** Connects to database **/
-    public function connect( $host, $username, $password, $dbname )
+	/** Connects to database **/
+	public function connect( $host, $username, $password, $dbname )
 	{
 		try
 		{
@@ -118,27 +135,37 @@ class PDOConn extends SQLHandle
 			$this->put_error( $e->getMessage() );
 			return false;
 		}
-		
+
 		$this->_dbHandle->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	    $this->_dbHandle->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
-		
+		$this->_dbHandle->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
+
 		$this->_isConn = true;
-    }
-    
-    public function isValid()
-    {
-    	return $this->_isConn;
-    }
+	}
+
+	public function isValid()
+	{
+		return $this->_isConn;
+	}
+
+	public function beginTransaction()
+	{
+		return $this->_dbHandle->beginTransaction();
+	}
+
+	public function commit()
+	{
+		return $this->_dbHandle->commit();
+	}
+
+	public function rollBack()
+	{
+		return $this->_dbHandle->rollBack();
+	}
 
 	public function clean( $string, $type = "str" )
 	{
-		if( get_magic_quotes_gpc() )
-		{
-			$toReturn = stripslashes( $toReturn );
-		}
-		
 		$toReturn = $this->_dbHandle->quote( $string, PDO::PARAM_STR );
-		
+
 		switch( $type )
 		{
 			case "bool":
@@ -152,24 +179,24 @@ class PDOConn extends SQLHandle
 				$toReturn = $toReturn;
 				break;
 		}
-		
+
 		return $toReturn;
 	}
-		
-	public function query( $query, $asObj, $params )
+
+	public function query( $query, $params )
 	{
 		$query = trim( $query );
-		
+
 		$results = new SQLResult();
-		
+
 		// no query called
 		if( $query == "" || !is_string( $query ) || $this->_dbHandle == null )
 		{
-			return ( $asObj == false ? false : $results );
+			return $results;
 		}
-		
+
 		$results->set_query( $query, $params );
-		
+
 		try
 		{
 			$stmt = $this->_dbHandle->prepare( $query );
@@ -177,11 +204,10 @@ class PDOConn extends SQLHandle
 		catch( PDOException $e )
 		{
 			$this->put_error( $e->getMessage() . " SQL STATEMENT:" . $query );
-			return ( $asObj == false ? false : $results );
+			return $results;
 		}
-		
+
 		$results->set_stmt( $stmt );
-		
 		if( $params != null && is_array( $params ) && !empty( $params ) )
 		{
 			// make sure they're all the same length
@@ -191,20 +217,20 @@ class PDOConn extends SQLHandle
 				if( !array( $param ))
 				{
 					$this->put_error( "Parameter " . $key . " is not an array. SQL STATEMENT:" . $query );
-					return ( $asObj == false ? false : $results );
+					return $results;
 				}
 				array_push( $lens, count( $param ) );
 			}
 			unset( $param, $key );
 			$lens = array_unique( $lens );
-			
+
 			if( count( $lens ) != 1 )
 			{
 				$this->put_error( "Statement contains both question mark and named placeholders. SQL STATEMENT:" . $query );
-				return ( $asObj == false ? false : $results );
+				return $results;
 			}
 			$len = $lens[0];
-			
+
 			// go through each param
 			for( $i = 1; $i <= count( $params ); $i++ )
 			{
@@ -212,33 +238,36 @@ class PDOConn extends SQLHandle
 				$parameter 	= $i;
 				$value 		= $params[$key][0];
 				$data_type 	= $params[$key][1];
-				
+
 				// named placeholders
 				if( $len == 3 )
 				{
 					$parameter 	= $params[$key][0];
 					$value 		= $params[$key][1];
 					$data_type 	= $params[$key][2];
-					
+
 					if( !in_array( $data_type, array( "bool", "null", "int", "str", "noquote" ) ) )
 					{
 						$data_type = "str";
 					}
 				}
-				
-				if( get_magic_quotes_gpc() )
+				try
 				{
-					$value = stripslashes( $value );
+					$test = $results->get_stmt()->bindValue( $parameter, $value, constant( "PDO::PARAM_" . strtoupper( $data_type ) ) );
+					if( $results->get_stmt()->bindValue( $parameter, $value, constant( "PDO::PARAM_" . strtoupper( $data_type ) ) ) === false )
+					{
+						$this->put_error( "Statement parameter " . $parameter . " ( " . $parameter . "," . $value . "," . $data_type . " ) is invalid. SQL STATEMENT:" . $query );
+						return $results;
+					}
 				}
-				
-				if( $results->get_stmt()->bindValue( $parameter, $value, constant( "PDO::PARAM_" . strtoupper( $data_type ) ) ) === false )
+				catch( Exception $e )
 				{
-					$this->put_error( "Statement parameter " . $parameter . " ( " . $parameter . "," . $value . "," . $data_type . " ) is invalid. SQL STATEMENT:" . $query );
-					return ( $asObj == false ? false : $results );
+					error_log($e);
 				}
-			}	
+
+			}
 		}
-		
+
 		try
 		{
 			$results->get_stmt()->execute();
@@ -246,24 +275,24 @@ class PDOConn extends SQLHandle
 		catch( PDOException $e )
 		{
 			$this->put_error( $e->getMessage() . " SQL STATEMENT:" . $query );
-			return ( $asObj == false ? false : $results );
+			return $results;
 		}
-		
+
 		$result = array();
 		$table = array();
 		$field = array();
 		$tempResults = array();
-				
+
 		if( preg_match( "/^select/im", $query ) && $results->get_stmt()->rowCount() > 0 )
 		{
 			$numOfFields = $results->get_stmt()->columnCount();
 			for( $i = 0; $i < $numOfFields; ++$i )
 			{
 				$meta = $results->get_stmt()->getColumnMeta( $i );
-				
+
 				array_push( $field, $meta['name'] );
-				
-				if( $asObj && empty( $meta['table'] ) )
+
+				if( empty( $meta['table'] ) )
 				{
 					array_push( $table, "fn" );
 				}
@@ -272,7 +301,7 @@ class PDOConn extends SQLHandle
 					array_push( $table, $meta['table'] );
 				}
 			}
-			
+
 			while( $row = $results->get_stmt()->fetch( PDO::FETCH_NUM ) )
 			{
 				for( $i = 0; $i < $numOfFields; ++$i )
@@ -283,48 +312,48 @@ class PDOConn extends SQLHandle
 				array_push( $result, $tempResults );
 			}
 		}
-		
+
 		$results->set_results( $result );
-		
-		return ( $asObj == false ? $results->as_array() : $results );
+
+		return $results;
 	}
-	
+
 	public function id()
 	{
 		return $this->_dbHandle->lastInsertId();
 	}
 
-    /** Get error string **/
-    public function error()
-    {
-        return ( $this->_dbHandle->errorCode() != "00000" );
-    }
-    
-     /** Get error number **/
-    public function errno()
-    {
-        return $this->_dbHandle->errorCode();
-    }
-    
-    /** Get error desc **/
-    public function errdesc()
-    {
-         return print_r( $this->_dbHandle->errorInfo(), true );
-    }
+	/** Get error string **/
+	public function error()
+	{
+		return ( $this->_dbHandle->errorCode() != "00000" );
+	}
+
+	 /** Get error number **/
+	public function errno()
+	{
+		return $this->_dbHandle->errorCode();
+	}
+
+	/** Get error desc **/
+	public function errdesc()
+	{
+		return print_r( $this->_dbHandle->errorInfo(), true );
+	}
 }
 
 class SQLResult
 {
 	protected $_isValid 		= false;
-	protected $_query 			= null;
-	protected $_params 			= null;
-	protected $_stmt 			= null;
-	protected $_results			= array();
-	protected $_pos 			= -1;
+	protected $_query 		= null;
+	protected $_params 		= null;
+	protected $_stmt 		= null;
+	protected $_results		= array();
+	protected $_pos 		= -1;
 	protected $_wasSerialized 	= false;
-	
+
 	public function __construct(){}
-	
+
 	public function __sleep()
 	{
 		return array(
@@ -335,42 +364,42 @@ class SQLResult
 			"_pos",
 		);
 	}
-	
+
 	public function __wakeup()
 	{
 		$this->_wasSerialized = true;
 	}
-	
+
 	/**
-	* 
+	*
 	* PUBLIC USE
-	* 
+	*
 	**/
 	public function isValid()
 	{
 		return $this->_isValid;
 	}
-	
+
 	public function first()
 	{
 		if( !isset( $this->_results[0] ) )
 		{
 			return false;
 		}
-		
+
 		return $this->_results[0];
 	}
-	
+
 	public function last()
 	{
 		if( $this->length() == 0 )
 		{
 			return false;
 		}
-		
+
 		return $this->_results[$this->length()-1];
 	}
-	
+
 	public function curr()
 	{
 		$pos = ( $this->_pos == -1 && $this->length() > 0 ? 0 : -1 );
@@ -378,10 +407,10 @@ class SQLResult
 		{
 			return false;
 		}
-		
+
 		return $this->_results[ $this->_pos ];
 	}
-	
+
 	public function next()
 	{
 		if( !isset( $this->_results[$this->_pos+1] ) )
@@ -390,17 +419,22 @@ class SQLResult
 		}
 		return $this->_results[++$this->_pos];
 	}
-	
+
 	public function prev()
 	{
 		if( !isset( $this->_results[$this->_pos-1] ) )
 		{
 			return false;
 		}
-		
+
 		return $this->_results[--$this->_pos];
 	}
-	
+
+	public function all()
+	{
+		return $this->_results;
+	}
+
 	public function reset()
 	{
 		$this->_pos = -1;
@@ -411,13 +445,13 @@ class SQLResult
 	{
 		return count( $this->_results );
 	}
-	
+
 	public function search( $table, $field = "" )
 	{
 		// get position, then go to the beginning
 		$pos = $this->_pos;
 		$this->reset();
-		
+
 		// store the values
 		$vals = array();
 		while( $row = $this->next() )
@@ -427,28 +461,60 @@ class SQLResult
 				array_push( $vals, $val );
 			}
 		}
-		
+
 		$this->_pos = $pos;
-		
+
 		return $vals;
 	}
-	
+
+	public function apply_function_to_cells( $table, $field, $functionName, $additionalParams = array() )
+	{
+		if( !function_exists( $functionName ) )
+		{
+			return false;
+		}
+
+		// get position, then go to the beginning
+		$pos = $this->_pos;
+		$this->reset();
+
+		// store the values
+		while( $row = $this->next() )
+		{
+			if( isset( $row->$table->$field ) && ( $val = $row->$table->$field ) != "" )
+			{
+				$newVal = call_user_func_array( $functionName, array_merge( array( $row->$table->$field ), $additionalParams ) );
+
+				if( $newVal === false )
+				{
+					return false;
+				}
+
+				$row->$table->$field = $newVal;
+			}
+		}
+
+		$this->_pos = $pos;
+
+		return true;
+	}
+
 	public function shuffle()
 	{
 		$this->results = shuffle( $this->_results );
-		
+
 		return $this;
 	}
-	
+
 	public function slice( $start, $end = -1 )
 	{
 		if( $end == -1 )
 		{
 			$end = $this->lenght();
 		}
-		
+
 		$this->reset();
-		
+
 		while( $row = $this->next() )
 		{
 			if( $this->position() < $start || $this->position() >= $end )
@@ -456,51 +522,67 @@ class SQLResult
 				unset( $this->_results[$this->position()] );
 			}
 		}
-		
+
 		$this->reset();
-		
+
 		return $this;
 	}
-	
+
 	public function position()
 	{
 		return $this->_pos;
 	}
-	
+
 	public function error()
 	{
 		if( $this->wasSerialized() && $this->isValid() )
 		{
 			return false;
 		}
-		else if( is_null( $this->_stmt ) )
+		else if(
+			is_null( $this->_stmt )
+			|| $this->has_statement_error()
+			|| $this->has_dbh_error()
+		)
 		{
 			return true;
 		}
-		
-		return ( $this->_stmt->errorCode() != "00000" );
+
+		return false;
 	}
-	
+
 	public function errno()
 	{
-		if( is_null( $this->_stmt ) )
+		/*
+		if( is_null( $this->_stmt ) || $this->has_dbh_error() )
 		{
-			return -1;
+			return Registry::get("_dbh")->errno();
 		}
-		
-		return $this->_stmt->errorCode();
+		else */
+		if( $this->has_statement_error() )
+		{
+			return $this->_stmt->errorCode();
+		}
+
+		return 0;
 	}
-	
+
 	public function errdesc()
 	{
-		if( is_null( $this->_stmt ) )
+		/*
+		if( is_null( $this->_stmt ) || $this->has_dbh_error() )
 		{
-			return "";
+			return Registry::get("_dbh")->errdesc();
 		}
-		
-		return print_r( $this->_stmt->errorInfo(), true );
+		else */
+		if( $this->has_statement_error() )
+		{
+			return print_r( $this->_stmt->errorInfo(), true );
+		}
+
+		return "";
 	}
-	
+
 	public function as_array()
 	{
 		$resultArray = array();
@@ -512,25 +594,25 @@ class SQLResult
 		return $resultArray;
 	}
 
-	
+
 	/**
-	* 
+	*
 	* SETTERS
-	* 
+	*
 	**/
 	public function set_query( $query, $params )
 	{
 		$this->_query = $query;
 		$this->_params = $params;
 	}
-	
+
 	public function set_stmt( $stmt )
 	{
 		if( get_class( $stmt ) != "PDOStatement" )
 		{
 			throw new Exception( "SQLResult: not a valid PDOStatement object" );
 		}
-		
+
 		$this->_stmt = $stmt;
 	}
 
@@ -546,40 +628,51 @@ class SQLResult
 			$row = new SQLRow( $result );
 			array_push($this->_results, $row );
 		}
-		
+
 		$this->_isValid = true;
 	}
-	
+
 	/**
-	* 
+	*
 	* GETTERS
-	* 
+	*
 	**/
 	public function get_query()
 	{
 		return $this->_query . " - Params: " . print_r( $params, true );
 	}
-	
+
 	public function get_stmt()
 	{
 		return $this->_stmt;
 	}
-	
+
 	public function __get( $key )
 	{
-		if( !isset( $this->$key ) )
+		if( !isset( $this->$key ) && $this->first() !== false )
 		{
 			return $this->first()->$key;
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	*
 	* PRIVATE
-	* 
+	*
 	**/
+	protected function has_statement_error()
+	{
+		return ( $this->_stmt->errorCode() != "00000" );
+	}
+
+	protected function has_dbh_error()
+	{
+		//return Registry::get("_dbh")->error();
+		return false;
+	}
+
 	protected function wasSerialized()
 	{
 		return $this->_wasSerialized;
@@ -590,22 +683,42 @@ class SQLRow
 {
 	protected $_row;
 
-	public function __construct( $arr )
+	public function __construct( $val )
 	{
-		$this->_row = $this->init( $arr );
+		$this->_row = $this->init( $val );
 	}
-	
+
+	public function __get( $key )
+	{
+		if( isset( $this->_row->$key ) || method_exists( $this->_row, $key ) )
+		{
+			return $this->_row->$key;
+		}
+
+		return null;
+	}
+
+	public function __set( $key, $value )
+	{
+		$this->_row->$key = $value;
+	}
+
 	private function init( $arr )
 	{
-		if( is_array( $arr ) ) 
+		if( $arr instanceof SQLRow || $arr instanceof SQLResult )
+		{
+			return $arr;
+		}
+
+		if( is_array( $arr ) )
 		{
 			$arr = (object) $arr;
 		}
-		
+
 		if( is_object( $arr ) )
 		{
 			$new = new stdClass();
-			
+
 			foreach( $arr as $key => $val )
 			{
 				$new->{$key} = $this->init( $val );
@@ -615,29 +728,38 @@ class SQLRow
 		{
 			$new = $arr;
 		}
-		
+
 		return $new;
 	}
-	
-	public function __get( $key )
+
+	public function row()
 	{
-		if( !isset( $this->_row->$key ) )
-		{
-			$this->_row->$key = $this->init( array() );
-		}
-		
-		return $this->_row->$key;
+		return $this->_row;
 	}
-	
+
 	public function as_array()
 	{
+		return $this->internal_as_array( $this->_row );
+	}
+
+	protected function internal_as_array( $vals )
+	{
 		$array = array();
-		
-		foreach( $this->_row as $key => $value )
+
+		foreach( $vals as $key => $value )
 		{
+			if( method_exists( $value, "as_array" ) )
+			{
+				$value = $value->as_array();
+			}
+			else if( is_array( $value ) )
+			{
+				$value = $this->internal_as_array( $value );
+			}
+
 			$array[$key] = (array) $value;
 		}
-		
+
 		return $array;
 	}
 }
